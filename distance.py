962b987884c2fa971a5bc859fec810047422f4e8
soltanoff <piccadillable@gmail.com>
2018-05-11 00:22:17 +0300

    refactor

--- distance.py
+++ distance.py
@@ -11,5 +11,4 @@ def point_distance(a, b):
 def route_distance(cities):
     """Возвращает длинну маршрута"""
     points = cities[['x', 'y']]
-    distances = point_distance(points, np.roll(points, 1, axis=0))
-    return np.sum(distances)
+    return np.sum(point_distance(points, np.roll(points, 1, axis=0)))
