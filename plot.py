107cdc66a0b1ea65a86c72eba94cefaaa84ba100
soltanoff <piccadillable@gmail.com>
2018-05-10 18:12:29 +0300

    fix: rendering over existing records

--- plot.py
+++ plot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -6,33 +8,45 @@ def plot_population(cities, population, name='diagram.png', ax=None):
     """Отрисовка всей входящей популяции в файл"""
     mpl.rcParams['agg.path.chunksize'] = 10000
 
-    fig = plt.figure(figsize=(5, 5), frameon=False)
-    axis = fig.add_axes([0, 0, 1, 1])
+    if not ax:
+        fig = plt.figure(figsize=(5, 5), frameon=False)
+        axis = fig.add_axes([0, 0, 1, 1])
 
-    axis.set_aspect('equal', adjustable='datalim')
-    plt.axis('off')
+        axis.set_aspect('equal', adjustable='datalim')
+        plt.axis('off')
 
-    axis.scatter(cities['x'], cities['y'], color='red', s=4)
-    axis.plot(population[:, 0], population[:, 1], 'r.', ls='-', color='#0063ba', markersize=2)
+        axis.scatter(cities['x'], cities['y'], color='red', s=4)
+        axis.plot(population[:, 0], population[:, 1], 'r.', ls='-', color='#0063ba', markersize=2)
 
-    plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
-    plt.close()
+        plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
+    else:
+        ax.scatter(cities['x'], cities['y'], color='red', s=4)
+        ax.plot(population[:, 0], population[:, 1], 'r.', ls='-', color='#0063ba', markersize=2)
+        return ax
 
 
 def plot_route(cities, route, name='diagram.png', ax=None):
     """Отрисовка маршрута"""
     mpl.rcParams['agg.path.chunksize'] = 10000
 
-    fig = plt.figure(figsize=(5, 5), frameon=False)
-    axis = fig.add_axes([0, 0, 1, 1])
+    if not ax:
+        fig = plt.figure(figsize=(5, 5), frameon=False)
+        axis = fig.add_axes([0, 0, 1, 1])
 
-    axis.set_aspect('equal', adjustable='datalim')
-    plt.axis('off')
+        axis.set_aspect('equal', adjustable='datalim')
+        plt.axis('off')
 
-    axis.scatter(cities['x'], cities['y'], color='red', s=4)
-    route = cities.reindex(route)
-    route.loc[route.shape[0]] = route.iloc[0]
-    axis.plot(route['x'], route['y'], color='purple', linewidth=1)
+        axis.scatter(cities['x'], cities['y'], color='red', s=4)
+        route = cities.reindex(route)
+        route.loc[route.shape[0]] = route.iloc[0]
+        axis.plot(route['x'], route['y'], color='purple', linewidth=1)
 
-    plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
-    plt.close()
+        plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
+    else:
+        ax.scatter(cities['x'], cities['y'], color='red', s=4)
+        route = cities.reindex(route)
+        route.loc[route.shape[0]] = route.iloc[0]
+        ax.plot(route['x'], route['y'], color='purple', linewidth=1)
+        return ax
